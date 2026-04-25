b6191804a388cca205b477c2a502f0459b74d32e
Just Winter <95353970+CrimsonDisk@users.noreply.github.com>
2026-04-26 02:28:01 +0700

    Working prototype with a few additions
    A working prototype with a brick falling down, and an addition is the support of music (yay!!).
    This prototype only has the falling and bottom border check.
    Window scale should work too.

--- scr/GamePanel.java
+++ scr/GamePanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.GraphicAttribute;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
@@ -8,9 +9,9 @@ import javax.sound.sampled.*;
 public class GamePanel extends JPanel implements Runnable {
     //---[Main window and scaling stuff]---
     private int winScale;
-    private int cols = 10;
-    private int rows = 20;
-    private int startCol = 5;
+    private int cols = 13;
+    private int rows = 21;
+    private int startCol = 6;
     private int brickPixel = 13;
 
     private int screenWidth = 13 * cols;
@@ -20,54 +21,66 @@ public class GamePanel extends JPanel implements Runnable {
     private int brickX = brickPixel *  startCol;
     private int brickY = 0;
     private int frameCounter = 0;
-    private BufferedImage bufferedImage;
+    private BufferedImage brickImg;
 
     public GamePanel(int winScale) {
         this.winScale = winScale;
         this.setPreferredSize(new Dimension(screenWidth * winScale, screenHeight * winScale));
         this.setBackground(Color.BLACK);
 
+        loadTexture();
+        playBgMusic();
+
         Thread gameThread = new Thread(this);
         gameThread.start();
     }
 
+    //---[Loads brick textures]---
+    private void loadTexture() {
+        try { brickImg = ImageIO.read(new File("resources/textures/bricks/Purple Brick.png")); }
+        catch (Exception e) { System.out.println("Cannot load, possibly missing texture: " + e.getMessage()); }
+    }
 
+    //---[The Runnable stuff. The brick movement updates, and a somewhat stable 60 fps cap]---
+    @Override
+    public void run() {
+        while(true) {
+            frameCounter++;
+            if (frameCounter >= 2) {
+                if (brickY < screenHeight - brickPixel) {
+                    brickY++;
+                }
+                frameCounter = 0;
+            }
 
+            repaint();
+            try { Thread.sleep(16); }
+            catch (Exception e) {}
 
+        }
+    }
 
+    //---[The texturing stuff]---
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        if (brickImg != null) {
+            g2.drawImage(brickImg, brickX * winScale, (brickY - 6) * winScale, brickPixel * winScale, (brickPixel + 6) * winScale, null);
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //---[Background music!!!]---
+    private void playBgMusic() {
+        try {
+            File musicFile = new File("resources/music/Bad Apple!! (PJSK collab).wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musicFile);
+            Clip musicClip = AudioSystem.getClip();
+            musicClip.open(audioStream);
+            musicClip.loop(Clip.LOOP_CONTINUOUSLY);
+            musicClip.start();
+        }
+        catch (Exception e) { System.out.println("Audio Error:" + e.getMessage()); }
+    }
 
 
 
