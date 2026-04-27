c6a04f35a4fbff58ab711b6e29619b45f6d6d012
hohoanghung102007-cloud <hohoanghung102007@gmail.com>
2026-04-27 18:20:41 +0700

    update Main

--- scr/Main.java
+++ scr/Main.java
@@ -1,26 +1,42 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
+        
+        int scale = 2; 
+        String input = JOptionPane.showInputDialog(null, "Choose your window scale, 2 is recommended.");
+        
+        try {
+            if (input != null && !input.isEmpty()) {
+                scale = Integer.parseInt(input);
+            } else if (input == null) {
+                
+                System.exit(0);
+            }
+        } catch (NumberFormatException e) {
+            
+            JOptionPane.showMessageDialog(null, "Invalid input! Using default scale 2.");
+            scale = 2;
+        }
+
         
         JFrame window = new JFrame();
         
         
         window.setTitle("Project 4-13 alpha 0.5.1");
         
-        
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
         
-        GamePanel gamePanel = new GamePanel(3); 
-        
+        GamePanel gamePanel = new GamePanel(scale);
         
         window.add(gamePanel);
         window.pack(); 
 
-        window.setLocationRelativeTo(null); 
-        window.setVisible(true); 
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
         
         gamePanel.startGameThread();
